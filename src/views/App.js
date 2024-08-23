bcaf3dd78fcef74b1cf7c7e20776730c30e0a2db
vmt311 <vmttq311@gmail.com>
2024-08-23 22:40:26 +0700

    example component

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
 
+import MyComponent from './Example/MyComponent';
+
+
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>hello my component Thien</div>
+        )
+    }
+}
+
+export default MyComponent;
